b69fbb9c091d2c4d91577fcaa21bcd5363094645
karolina-v <vazniak96@gmail.com>
2022-01-21 12:47:06 +0200

    hw7

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,34 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+const galleryEl = document.querySelector('gallery');
+
+
+const newLinkEl = document.createElement('a');
+newLinkEl.classList.add('gallery_link');
+
+const newImgEl = document.createElement('img');
+newImgEl.classList.add('gallery_image');
+newLinkEl.append(newImgEl);
+
+const imagePreview = galleryItems.map(item => {
+    newLinkEl.href = item.original;
+    newImgEl.src = item.preview;
+    newImgEl.alt = item.description;
+});
+
+galleryEl.append(newImgEl);
+console.log(galleryEl);
+
+
+
+galleryEl.addEventListener('click', onImageClick);
+
+function onImageClick(e) {
+    e.preventDefault();
+    if (!e.target.classList.contains('gallery_image')) {
+    return;
+    }
+}
