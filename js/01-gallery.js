ce3d8fae0b5b8e19b84d03fc5e9bce317f8354f9
karolina-v <vazniak96@gmail.com>
2022-01-23 23:56:30 +0200

    hw7
    hw7

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,34 +1,57 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from './gallery-items.js';  
 
-// console.log(galleryItems);
-
-const galleryEl = document.querySelector('gallery');
-
-
-const newLinkEl = document.createElement('a');
-newLinkEl.classList.add('gallery_link');
-
-const newImgEl = document.createElement('img');
-newImgEl.classList.add('gallery_image');
-newLinkEl.append(newImgEl);
-
-const imagePreview = galleryItems.map(item => {
-    newLinkEl.href = item.original;
-    newImgEl.src = item.preview;
-    newImgEl.alt = item.description;
-});
-
-galleryEl.append(newImgEl);
-console.log(galleryEl);
-
-
+const galleryEl = document.querySelector('.gallery');
 
 galleryEl.addEventListener('click', onImageClick);
 
+
+function createImageCards (galleryItems) {
+    return galleryItems
+      .map(({ preview, original, description }) => {
+          return `<a class='gallery__link' href='${original}'>
+                       <img
+                       class='gallery__image'
+                       src='${preview}' 
+                       data-source="${original}" 
+                       alt='${description}'>
+                    </a>`;
+    })
+      .join('');
+}
+
+galleryEl.insertAdjacentHTML('beforeend', createImageCards(galleryItems));
+
+
+
 function onImageClick(e) {
     e.preventDefault();
-    if (!e.target.classList.contains('gallery_image')) {
-    return;
+
+    if (e.target.nodeName !== 'IMG') {
+        return;
     }
+
+
+    const imageLink = e.target.dataset.source;
+    const imageDescription = e.target.alt;
+    const imageToLightbox = `<img src ='${imageLink}', alt='${imageDescription}'>`;
+    
+    const options = {
+onShow: () => {window.addEventListener("keydown", onCloseEscape);
+},
+onClose: () => {window.removeEventListener("keydown", onCloseEscape);
+},
+    }
+
+   
+    const instance = basicLightbox.create(imageToLightbox, `${options}`);
+    instance.show();
+
+    function onCloseEscape(e) {
+        if (e.code === 'Escape') {
+            instance.close(options.onClose);
+        }
+         instance.show(options.onShow);
+    }
+    
 }
+ 
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,42 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from './gallery-items.js';  
 
-console.log(galleryItems);
+const galleryEl = document.querySelector('.gallery');
+
+galleryEl.addEventListener('click', onImageClick);
+
+
+function createImageCards (galleryItems) {
+    return galleryItems
+      .map(({ preview, original, description }) => {
+          return `<a class='gallery__item' href='${original}'>
+                       <img
+                       class='gallery__image'
+                       src='${preview}' 
+                       alt='${description}'>
+                    </a>`;
+    })
+      .join('');
+}
+
+galleryEl.insertAdjacentHTML('beforeend', createImageCards(galleryItems));
+
+
+
+function onImageClick(e) {
+    e.preventDefault();
+
+    if (e.target.nodeName !== 'IMG') {
+        return;
+    }
+
+    const lightbox = new SimpleLightbox('.gallery a', {
+        'captionType': 'atr',
+        'captionsData': 'alt',
+        'captionPosition': 'bottom',
+        'captionDelay': 250,
+
+    });
+
+    
+}
+ 
